3c808a665d1d809fbd03f674f1fc1110ae719fda
Bhushan Srivastava <84985241+bhushan-it@users.noreply.github.com>
2022-01-20 21:14:40 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ scrollContainer.addEventListener("wheel", (evt) => {
     evt.preventDefault();
     scrollContainer.scrollLeft = scrollContainer.scrollLeft + 20 * evt.deltaY;
 });*/
-var setTheme = localStorage.getItem('theme');
+var setTheme = window.localStorage.getItem('theme');
 if (setTheme == null) {
     document.getElementById("myStyleSheet").href = 'lightmode.css';
-    localStorage.setItem('theme', 'lightmode.css');
+    window.localStorage.setItem('theme', 'lightmode.css');
 }
 function change() {
-    var setTheme = localStorage.getItem('theme');
+    var setTheme = window.localStorage.getItem('theme');
     if (setTheme == 'darkmode.css') {
         document.getElementById("myStyleSheet").href = 'lightmode.css';
-        localStorage.setItem('theme', 'lightmode.css');
+        window.localStorage.setItem('theme', 'lightmode.css');
     }
     else if (setTheme == 'lightmode.css') {
         document.getElementById("myStyleSheet").href = 'darkmode.css';
-        localStorage.setItem('theme', 'darkmode.css');
+        window.localStorage.setItem('theme', 'darkmode.css');
     }
 }
 
@@ -94,6 +94,45 @@ function endFunction() {
 
 }
 
-
-
-
+const ball = document.getElementById('cursors');
+let aimX = 0;
+let aimY = 0;
+let currentX = 0;
+let currentY = 0;
+let speed = 0.3;
+function animate() {
+    currentX += (aimX - currentX) * speed;
+    currentY += (aimY - currentY) * speed;
+    ball.style.left = currentX + "px";
+    ball.style.top = currentY + "px";
+    requestAnimationFrame(animate);
+}
+animate();
+document.addEventListener("mousemove", function (event) {
+    aimX = event.pageX;
+    aimY = event.pageY;
+});
+function mouseOverFunction() {
+    if (window.innerHeight > 446 && window.innerWidth > 1009) {
+        document.getElementById('cursors').style.display = 'inherit';
+        document.getElementById('cursors').style.width = 32;
+        document.getElementById('cursors').style.height = 32;
+        document.getElementById('cursors').style.border = 'solid 2px white';
+        document.getElementById('cursors').style.backgroundColor = 'rgba(255, 255, 255, 0)';
+    }
+    else {
+        document.getElementById('cursors').style.display = 'none';
+    }
+}
+function mouseOutFunction() {
+    if (window.innerHeight > 446 && window.innerWidth > 1009) {
+        document.getElementById('cursors').style.display = 'inherit';
+        document.getElementById('cursors').style.width = 10;
+        document.getElementById('cursors').style.height = 10;
+        document.getElementById('cursors').style.border = 'none';
+        document.getElementById('cursors').style.backgroundColor = '#111';
+    }
+    else {
+        document.getElementById('cursors').style.display = 'none';
+    }
+}
